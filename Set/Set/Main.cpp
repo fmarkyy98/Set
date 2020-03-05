a956c215f5da75f8ac42fab9b86a67e1f5fd382f
Fekete Márk <fmark98@gmail.com>
2020-03-06 00:10:08 +0100

    Main developed for using it via commands.

--- Set/Set/Main.cpp
+++ Set/Set/Main.cpp
@@ -5,68 +5,82 @@ using namespace std;
 
 int main()
 {
-	Set ps;
-	string help = "Add to set: \"a <whole value>\"\nExample:\n>a 8\n\nRemove from set: \"r <whole value>\"\nExample:\n>r 8\n\nDecide that, the Set contains the given value: \"c <whole value>\"\nExample:\n>c 8\n\nType \"help\" to show help message\n\nType \"END\" to terminate program\n\n";
-	cout << help;
+	Set s;
+	string addHelp = "Add to set: \"a <whole value>\"\nExample:\n>a 8\n\n";
+	string removeHelp = "Remove from set: \"r <whole value>\"\nExample:\n>r 8\n\n";
+	string containsHelp = "Decide that, the Set contains the given value : \"c <whole value>\"\nExample:\n>c 8\n\n";
+	string evenHelp = "Write out, the count of the even element in the Set : \"e\"\nExample:\n>e\n\n";
+	string help = "Type \"help\" to show help message\n\n";
+	string END = "Type \"END\" to terminate program\n\n";
+
+	cout << addHelp << removeHelp << containsHelp << evenHelp << help << END;
 	string cmdString;
 	do
 	{
+		cout << "Your Set: " << s << endl;
 		cout << ">";
 		cin >> cmdString;
 		if (cmdString == "a")
 		{
 			int value;
 			cin >> value;
-			cout << value << endl;
+			try
+			{
+				s.add(value);
+			}
+			catch (Set::Exception e)
+			{
+				switch (e)
+				{
+				case Set::alreadyContainsValueException:
+					cout << "The set already contains " << value << endl;
+					break;
+				}
+			}
 		}
 		else if (cmdString == "r")
 		{
 			int value;
 			cin >> value;
-			cout << value << endl;
+			try
+			{
+				s.remove(value);
+			}
+			catch (Set::Exception e)
+			{
+				switch (e)
+				{
+				case Set::doesNotContainValueException:
+					cout << "The set does not contain " << value << endl;
+					break;
+				}
+			}
+		}
+		else if (cmdString == "c")
+		{
+			int value;
+			cin >> value;
+			cout << "Your Set " << (s.contains(value) ? "contains " : "does not contains ") << value << endl;
+		}
+		else if (cmdString == "e")
+		{
+			cout << "Your Set contains " << s.getEvenValuesCount() << " even value(s)" << endl;
+		}
+		else if (cmdString == "help")
+		{
+			cout << addHelp << removeHelp << containsHelp << help << END;
+		}
+		else
+		{
+			if (cmdString != "END")
+			{
+
+				cout << "Unknown command, for help type \"help\"";
+			}
 		}
 	} while (cmdString != "END");
-	
-	vector<int> values{ 2,3,5,7,12,13 };
-	Set ps2(values);
-
-	ps.add(2);
-	ps.add(3);
-	ps.add(5);
-	try
-	{
-		ps.add(5);
-	}
-	catch (Set::Exception e)
-	{
-		switch (e)
-		{
-		case Set::alreadyContainsValueException:
-			cout << "The set already contains 5\n";
-			break;
-		}
-	}
-	ps.add(7);
-	ps.add(11);
-	cout << ps << ps.getEvenValuesCount() << endl;
-	ps.remove(5);
-	try
-	{
-		ps.remove(5);
-	}
-	catch (Set::Exception e)
-	{
-		switch (e)
-		{
-		case Set::doesNotContainValueException:
-			cout << "The set does not contain 5\n";
-			break;
-		}
-	}
-	cout << ps << ps.getEvenValuesCount() << endl;
-	cout << ps2 << ps2.getEvenValuesCount() << endl;
-	cout << ps2.contains(5) << endl;
 
 	cout << "Hello World!\n";
+
 	return 0;
 }
